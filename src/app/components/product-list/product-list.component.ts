1ebc4e374017a61147f0ae61d274fd2fe29096b2
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-01-04 15:19:15 -0300

    Cambiando 'css' a 'scss'

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Product } from 'src/app/models/product.model';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
