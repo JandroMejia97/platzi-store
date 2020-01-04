1e9073fe5dd4a04ff6aad7ba3792602f865890c4
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-01-04 15:16:40 -0300

    Añadiendo componente para la lista de productos

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectedProduct(id: number) {
+    console.log(id);
+  }
+
+}
