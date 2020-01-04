f9a4ed24848c4d680270cb5b545af7c6a2b61dd1
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-01-04 19:41:07 -0300

    Modularizando en SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,33 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
-import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { ContactComponent } from './components/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { HomeModule } from './components/home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    ExponentialPipe,
-    HighlightDirective,
     ContactComponent,
-    HeaderComponent,
-    FooterComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
     LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
